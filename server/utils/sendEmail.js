ef728c9e8a4235429d9dc7d4f7083883c5d576da
shruu1403 <shruti.gupta01412.com>
2025-11-08 11:23:10 +0530

    Updated

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -28,12 +28,19 @@ const transporter = nodemailer.createTransport({
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail({ to, subject, html }) {
-  await resend.emails.send({
-    from: `"SplitMate" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+  try {
+    await resend.emails.send({
+      from: `"SplitMate" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+    console.log("✅ Email sent to", to);
+
+  } catch (error) {
+    console.error("❌ Error sending email", error);
+    throw error;
+  }
 }
 
 module.exports = { sendEmail };

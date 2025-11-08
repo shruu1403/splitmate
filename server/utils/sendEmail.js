9fdda261eb1df7d1aa18c6ff209146e558913151
shruu1403 <shruti.gupta01412.com>
2025-11-08 10:06:28 +0530

    Updated smtp to resend

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,27 +1,39 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config()
+const { Resend } = require("resend");
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail", 
+  service: "Gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
+// async function sendEmail({ to, subject, html }) {
+//   try {
+//     await transporter.sendMail({
+//       from: `"SplitMate" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     });
+//     console.log("✅ Email sent to", to);
+//   } catch (err) {
+//     console.error("❌ Error sending email", err);
+//     throw err;
+//   }
+// }
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 async function sendEmail({ to, subject, html }) {
-  try {
-    await transporter.sendMail({
-      from: `"SplitMate" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-    console.log("✅ Email sent to", to);
-  } catch (err) {
-    console.error("❌ Error sending email", err);
-    throw err;
-  }
+  await resend.emails.send({
+    from: `"SplitMate" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
 }
 
-module.exports = {sendEmail};
+module.exports = { sendEmail };

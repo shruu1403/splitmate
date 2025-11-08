26adb0af0d6d778ca823689b2bd5831bf1108460
shruu1403 <shruti.gupta01412.com>
2025-11-08 11:32:00 +0530

    Updated Email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -30,7 +30,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail({ to, subject, html }) {
   try {
     await resend.emails.send({
-      from: `"SplitMate" <${process.env.EMAIL_USER}>`,
+      from: `"SplitMate" <onboarding@resend.dev>`,
       to,
       subject,
       html,
